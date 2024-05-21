a92eae5d269c5891e186e71bc3d66cf630e95200
Brymonga4 <150121664+Brymonga4@users.noreply.github.com>
2024-05-21 21:59:50 +0200

    Added Lombock

--- src/main/java/org/example/model/Movie.java
+++ src/main/java/org/example/model/Movie.java
@@ -1,13 +1,17 @@
 package org.example.model;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import org.hibernate.annotations.NotFound;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data  // Esta anotación incluye @ToString, @EqualsAndHashCode, @Getter en todos los campos, @Setter en todos los campos no finales, y @RequiredArgsConstructor
+@NoArgsConstructor  // Genera un constructor sin argumentos
+@AllArgsConstructor // Genera un constructor con un argumento para cada campo en la clase
 @Entity
 @Table(name = "movies")
 public class Movie {
@@ -15,32 +19,35 @@ public class Movie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "movie_id")
     private Long id;
+
     @NotBlank(message = "El titulo no puede estar vacío.")
-    @Size(max = 100, message = "El título no puede tener más de 100 carácteres.")
+    @Size(max = 100, message = "El título no puede tener más de 100 caracteres.")
     @Column(nullable = false)
     private String title;
+
     @Column(name = "orig_title", nullable = false)
     private String origTitle;
+
     @Column(name = "release_date", nullable = false)
     private LocalDate release;
 
     @Column(nullable = false)
     private String genres;
+
     @Column(nullable = false)
     private String actors;
 
-    @Column( nullable = false)
+    @Column(nullable = false)
     private String directors;
 
     @Column(nullable = false)
     private String script;
 
-    @Column( nullable = false)
+    @Column(nullable = false)
     private String producers;
 
     @Column(nullable = false)
     private String synopsis;
-
 
     @Column(name = "original_version", nullable = false)
     private boolean originalVersion;
@@ -59,177 +66,4 @@ public class Movie {
 
     @Column(nullable = false)
     private Integer duration;
-
-    public Movie() {
-    }
-
-    public Movie(Long id, String title, String origTitle, LocalDate release, String genres, String actors, String directors, String script, String producers, String synopsis, boolean originalVersion, boolean spanishVersion, String image, String trailer, String ageRating, Integer duration) {
-        this.id = id;
-        this.title = title;
-        this.origTitle = origTitle;
-        this.release = release;
-        this.genres = genres;
-        this.actors = actors;
-        this.directors = directors;
-        this.script = script;
-        this.producers = producers;
-        this.synopsis = synopsis;
-        this.originalVersion = originalVersion;
-        this.spanishVersion = spanishVersion;
-        this.image = image;
-        this.trailer = trailer;
-        this.ageRating = ageRating;
-        this.duration = duration;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getOrigTitle() {
-        return origTitle;
-    }
-
-    public void setOrigTitle(String originalTitle) {
-        this.origTitle = originalTitle;
-    }
-
-    public LocalDate getRelease() {
-        return release;
-    }
-
-    public void setRelease(LocalDate release) {
-        this.release = release;
-    }
-
-    public String getGenres() {
-        return genres;
-    }
-
-    public void setGenres(String genres) {
-        this.genres = genres;
-    }
-
-    public String getActors() {
-        return actors;
-    }
-
-    public void setActors(String actors) {
-        this.actors = actors;
-    }
-
-    public String getDirectors() {
-        return directors;
-    }
-
-    public void setDirectors(String directors) {
-        this.directors = directors;
-    }
-
-    public String getScript() {
-        return script;
-    }
-
-    public void setScript(String script) {
-        this.script = script;
-    }
-
-    public String getProducers() {
-        return producers;
-    }
-
-    public void setProducers(String producers) {
-        this.producers = producers;
-    }
-
-    public String getSynopsis() {
-        return synopsis;
-    }
-
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
-    }
-
-    public boolean isOriginalVersion() {
-        return originalVersion;
-    }
-
-    public void setOriginalVersion(boolean originalVersion) {
-        this.originalVersion = originalVersion;
-    }
-
-    public boolean isSpanishVersion() {
-        return spanishVersion;
-    }
-
-    public void setSpanishVersion(boolean spanishVersion) {
-        this.spanishVersion = spanishVersion;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getTrailer() {
-        return trailer;
-    }
-
-    public void setTrailer(String trailer) {
-        this.trailer = trailer;
-    }
-
-    public String getAgeRating() {
-        return ageRating;
-    }
-
-    public void setAgeRating(String ageRating) {
-        this.ageRating = ageRating;
-    }
-
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public void setDuration(Integer duration) {
-        this.duration = duration;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", originalTitle='" + origTitle + '\'' +
-                ", release=" + release +
-                ", genres='" + genres + '\'' +
-                ", actors='" + actors + '\'' +
-                ", directors='" + directors + '\'' +
-                ", script='" + script + '\'' +
-                ", producers='" + producers + '\'' +
-                ", synopsis='" + synopsis + '\'' +
-                ", originalVersion=" + originalVersion +
-                ", spanishVersion=" + spanishVersion +
-                ", image='" + image + '\'' +
-                ", trailer='" + trailer + '\'' +
-                ", ageRating='" + ageRating + '\'' +
-                ", duration=" + duration +
-                '}';
-    }
 }
